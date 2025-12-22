7c5a912ae922a4fd700b664ad34b5948b6c563d6
Julian <jenunezb@uqvirtual.edu.co>
2025-12-21 23:21:38 -0500

    PRUEBA HealthController

---
+++ src/main/java/proyecto/controladores/HealthController.java
@@ -0,0 +1,13 @@
+package proyecto.controladores;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "OK";
+    }
+}
